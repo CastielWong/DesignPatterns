1972ef4f173786bff961db629626414760db0898
Castiel <castielwongsysu@gmail.com>
2020-05-09 23:16:09 +0930

    Add pattern State

---
+++ src/State/model/GumballMachine.java
@@ -0,0 +1,87 @@
+package State.model;
+
+import State.definition.State;
+import State.implementation.HasQuarterState;
+import State.implementation.NoQuarterState;
+import State.implementation.SoldOutState;
+import State.implementation.SoldState;
+
+public class GumballMachine {
+    private State soldOutState;
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+
+    private State state = soldOutState;
+    private int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        this.soldOutState = new SoldOutState(this);
+        this.noQuarterState = new NoQuarterState(this);
+        this.hasQuarterState = new HasQuarterState(this);
+        this.soldState = new SoldState(this);
+
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void insertQuarter() {
+        this.state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        this.state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        this.state.turnCrank();
+        state.dispense();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count - 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("\nMighty Gumball, Inc.");
+        result.append("\nJava-enabled Standing Gumball Model #2004");
+        result.append("\nInventory: " + count + " gumball");
+        if (count != 1) {
+            result.append("s");
+        }
+        result.append("\n");
+        result.append("Machine is " + state + "\n");
+        return result.toString();
+    }
+}
